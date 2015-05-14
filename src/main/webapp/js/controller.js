e36403227fd6b0d12773e34a1366c3b054b353af
kiksebegolovy <sasha25121993@mail.ru>
2015-05-14 21:02:20 +0300

    image filter

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -2,4 +2,12 @@ app.controller("Controller", function ($scope, FileUploader) {
     var uploader = $scope.uploader = new FileUploader({
         url: '/upload'
     });
+
+    uploader.filters.push({
+        name: 'imageFilter',
+        fn: function(item /*{File|FileLikeObject}*/, options) {
+            var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+            return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+        }
+    });
 });
